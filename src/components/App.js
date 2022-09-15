474037e15c8094b8d8fa894b9a3016ed8166cf9c
Raunak Sharma <98183441+Sharma572@users.noreply.github.com>
2022-09-15 19:08:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,3 @@
-
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
-
-  // do not change the code inside the function clickA
-  const clickA = () =>{
-    console.log('Clicked button A')
-  }
-  return (
-    <div id="main">
-      <button id="button-a" onClick={clickA}>Button A</button>
-    </div>
-  )
-}
-
-
-export default App;
-
-char guesser:
-
 import React, { Fragment, useEffect, useState } from "react";
 import "../styles/App.css";
 
